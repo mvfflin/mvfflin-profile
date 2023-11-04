747c3195741561501ea2ea333aecc4f72fab61b8
fatih <81780698+mvfflin@users.noreply.github.com>
2023-11-04 14:01:27 +0700

    feat: add contact section (#1)
    Co-authored-by: mvfflin <mynameisfatihul@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { HomeSection } from "@/components/home";
 import { AboutSection } from "@/components/about";
 import { StacksSection } from "@/components/stacks";
 import { ProjectSection } from "@/components/projects";
+import { ContactSection } from "@/components/contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function Home() {
           <AboutSection />
           <StacksSection />
           <ProjectSection />
+          <ContactSection />
         </div>
         <footer className="bottom-0 fixed font-bold p-5 dark:text-white">
           Copyright Mvfflin &copy; 2023
